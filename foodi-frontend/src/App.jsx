ae62e1b93957fd50a6d57a187980ca39240d38ef
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-08-26 03:29:17 +0600

    Created Routes

--- foodi-frontend/src/App.jsx
+++ foodi-frontend/src/App.jsx
@@ -1,12 +1,21 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 function App() {
 
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/cart' element ={<Cart></Cart>}></Route>
+        <Route path='/order' element ={<PlaceOrder></PlaceOrder>}></Route>
+      </Routes>
     </div>
   )
 }
