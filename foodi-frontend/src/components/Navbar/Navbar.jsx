c907a40ae403484cf8fd5d087078128214b4811e
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-08-26 02:55:47 +0600

    Navbar Component is created

--- foodi-frontend/src/components/Navbar/Navbar.jsx
+++ foodi-frontend/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,25 @@
+import { useState } from 'react';
 import './Navbar.css'
 import { FaBitbucket, FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
+    const [menu, setMenu] = useState("Home");
     return (
         <div className='flex justify-between items-center'>
             <div>
                 <h1>FooDi</h1>
             </div>
             <ul className='flex gap-10'>
-                <li>Home</li>
-                <li>Menu</li>
-                <li>Mobile-app</li>
-                <li>Contact us</li>
+                <li onClick={()=>{setMenu("Home")}} className={menu === "Home"? "active" : ""}>Home</li>
+                <li onClick={()=>{setMenu("Menu")}} className={menu === "Menu"? "active" : ""}>Menu</li>
+                <li onClick={()=>{setMenu("Mobile-app")}} className={menu === "Mobile-app"? "active" : ""}>Mobile-app</li>
+                <li onClick={()=>{setMenu("Contact us")}} className={menu === "Contact us"? "active" : ""}>Contact us</li>
             </ul>
             <div className='flex justify-center items-center gap-10'>
                 <FaSearch></FaSearch>
                 <FaBitbucket></FaBitbucket>
                 <div>
-                    <p>Sign In</p>
+                    <button className='border-2 rounded-3xl px-5 py-2 bg-transparent cursor-pointer duration-200 hover:bg-slate-200'>Sign In</button>
                 </div>
                 
             </div>
