bb0c1aab17637fd56b4b51874c825083f9a9736b
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-09-09 21:05:46 +0600

    User Controller is completed

--- foodi-backend/server.js
+++ foodi-backend/server.js
@@ -2,6 +2,11 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/database.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 
 const app = express();
@@ -11,12 +16,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
 //Database Connection
 connectDB();
 
 //api endpoints
 app.use("/api/food", foodRouter)
 app.use("/images", express.static("uploads"))
+app.use("/api/user", userRouter)
 
 app.get("/", (req, res)=>{
     res.send("Server is Working!!");
