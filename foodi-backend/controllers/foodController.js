80c1a6b39d9ddc1a50f00aa8dba7aa83bbb874b0
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-09-10 01:20:47 +0600

    add user sell food option

--- foodi-backend/controllers/foodController.js
+++ foodi-backend/controllers/foodController.js
@@ -21,6 +21,25 @@ const addFood = async (req, res) => {
         res.json({ success: false, message: "Error" })
     }
 }
+//Post food items
+const postFood = async (req, res) => {
+
+    let image_filename = `${req.file.filename}`;
+    const food = new foodModel({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        category: req.body.category,
+        image: image_filename
+    })
+    try {
+        await food.save();
+        res.json({ success: true, message: "Food Added" })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: "Error" })
+    }
+}
 
 //all the food list
 const listFood = async (req, res) => {
@@ -52,4 +71,5 @@ export {
     addFood,
     listFood,
     removeFood,
+    postFood,
 }
