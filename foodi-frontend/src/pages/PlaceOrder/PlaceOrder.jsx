d62bf1295d29f98bcdceb00aa1ac089380056ad0
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-09-30 19:13:09 +0600

    Completed payment process

--- foodi-frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ foodi-frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,27 +1,75 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 const PlaceOrder = () => {
-    const { getTotalCartAmount } = useContext(StoreContext);
+    const { getTotalCartAmount, token, food_list, cartItems, url} = useContext(StoreContext);
+    const [data, setData] = useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+        city:"",
+        street:"",
+        phone:"",
+
+    })
+
+    const onChangeHandler = (event)=>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data=>({...data, [name]:value}))
+    }
+    
+    // useEffect(()=>{
+    //     console.log(data);
+    // },[data])
+
+    const handlePlaceOrder = async(event)=>{
+        event.preventDefault();
+        let orderItems = [];
+        food_list.map(item =>{
+            if(cartItems[item._id]> 0){
+                let itemInfo = item;
+                itemInfo["quantity"] = cartItems[item._id];
+                orderItems.push(itemInfo)
+            }
+        })
+        // console.log(orderItems);
+        let orderData = {
+            address:data,
+            items:orderItems,
+            amount:(getTotalCartAmount() * 0.2).toFixed(2),
+
+        }
+        let response = await axios.post(url+"/api/order/place", orderData, {headers:{token}});
+        if(response.data.success){
+            const {session_url} = response.data;
+            window.location.replace(session_url);
+        }
+        else{
+            alert("Error");
+        }
+    }
+
     return (
-        <form className='px-2 mt-8'>
+        <form onSubmit={handlePlaceOrder} className='px-2 mt-8'>
             <div className='flex flex-col justify-between gap-10 md:flex-row'>
                 <div className='flex flex-col w-full'>
                     <p className='font-semibold'>Delivery Information</p>
                     <div className='flex justify-between'>
-                        <input type="text" placeholder='First name' className='border-2 border-gray-200 rounded-md p-1 mb-1  w-full' name="" id="" />
-                        <input type="text" placeholder='Last name' className='border-2 border-gray-200 rounded-md p-1 mb-1 ml-1 w-full' name="" id="" />
+                        <input required onChange={onChangeHandler} value={data.firstName} type="text" placeholder='First name' className='border-2 border-gray-200 rounded-md p-1 mb-1  w-full' name="firstName" id="" />
+                        <input required onChange={onChangeHandler} value={data.lastName} type="text" placeholder='Last name' className='border-2 border-gray-200 rounded-md p-1 mb-1 ml-1 w-full' name="lastName" id="" />
                     </div>
 
-                    <input type="email" placeholder='email' className='border-2 border-gray-200 rounded-md p-1 mb-1' name="" id="" />
+                    <input required onChange={onChangeHandler} value={data.email} type="email" placeholder='email' className='border-2 border-gray-200 rounded-md p-1 mb-1' name="email" id="" />
                     <div className='flex justify-between'>
-                        <input type="text" placeholder='City' className='border-2 border-gray-200 rounded-md p-1 mb-1 w-full' name="" id="" />
-                        <input type="text" placeholder='Street' className='border-2 border-gray-200 rounded-md p-1 mb-1 ml-1 w-full' name="" id="" />
+                        <input required onChange={onChangeHandler} value={data.city} type="text" placeholder='City' className='border-2 border-gray-200 rounded-md p-1 mb-1 w-full' name="city" id="" />
+                        <input required onChange={onChangeHandler} value={data.street} type="text" placeholder='Street' className='border-2 border-gray-200 rounded-md p-1 mb-1 ml-1 w-full' name="street" id="" />
 
                     </div>
 
-                    <input type="text" className='border-2 border-gray-200 rounded-md p-1 mb-1' placeholder='phone' name="" id="" />
+                    <input required onChange={onChangeHandler} value={data.phone} type="text" className='border-2 border-gray-200 rounded-md p-1 mb-1' placeholder='phone' name="phone" id="" />
 
 
                 </div>
@@ -39,7 +87,7 @@ const PlaceOrder = () => {
                         <p className='font-semibold'>Total</p>
                         <p>${parseFloat(getTotalCartAmount() + getTotalCartAmount() * 0.2).toFixed(2)}</p>
                     </div>
-                    <button className='border-2 bg-orange-600 text-white p-1 mt-2 rounded-md text-sm w-full'>PROCEED TO PAYMENT</button>
+                    <button type='submit' className='border-2 bg-orange-600 text-white p-1 mt-2 rounded-md text-sm w-full'>PROCEED TO PAYMENT</button>
                 </div>
             </div>
         </form>
